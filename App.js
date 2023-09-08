2fc59c69c1779613c153ad3d40a935932850e74b
Graydude98 <rodericksimani@yahoo.com>
2023-09-08 15:02:04 -0400

    replaced colons with commas for appjs

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import rough from 'roughjs/bundled/rough.esm';
 const generator = rough.generator();
 
 const App = () => {
-    const [elements, setElemnts] = useState(initialState: []);
+    const [elements, setElemnts] = useState(initialState, []);
 
 
 
@@ -15,8 +15,8 @@ const App = () => {
         const ctx = canvas.getContext("2d");
 
         const roughCanvas = rough.canvas(canvas);
-        const rect = generator.rectangle(x: 10, y: 10, width: 100, height: 100);
-        const line = generator.line(x1: 10, y1: 10, x2: 110, y2: 110);
+        const rect = generator.rectangle(x, 10, y, 10, width, 100, height, 100);
+        const line = generator.line(x1, 10, y1, 10, x2, 110, y2, 110);
         roughCanvas.draw(rect);
 
     });
