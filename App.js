82b680f33a44d9f44faaf23f2e79a0fac682148c
Graydude98 <rodericksimani@yahoo.com>
2023-09-11 14:00:41 -0400

    added more to App.js

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import rough from 'roughjs/bundled/rough.esm';
 
 const generator = rough.generator();
 
+function createElement(x1, y1, x2, y2) {
+    const roughElement = generator.line(x1, y1, x2, y2);
+    return { x1, y1, x2, y2, roughElement };
+}
+
 const App = () => {
     const [elements, setElemnts] = useState(initialState, []);
-
+    const [drawing, setDrawing] = useState( initialState, false);
 
 
 
@@ -13,18 +18,46 @@ const App = () => {
     useLayoutEffect(effect, () => {
         const canvas = document.getElementById("canvas");
         const ctx = canvas.getContext("2d");
+        context.clearRect(x,0, y,0, canvas.width, canvas.height);
 
         const roughCanvas = rough.canvas(canvas);
+
         const rect = generator.rectangle(x, 10, y, 10, width, 100, height, 100);
         const line = generator.line(x1, 10, y1, 10, x2, 110, y2, 110);
         roughCanvas.draw(rect);
-
+        roughCanvas.draw(line);
     });
+
+    const handleMouseDown = (event) => {
+        setDrawing(value, true);
+
+        const { clientX, clientY } = event; 
+
+        const element = createElement(clientX, clientY, clientX, clientY);
+        setElemnts( value, prevState => [...prevState, element]);
+    };
+
+    const handleMouseMove = (event) => {
+        if(!drawing) return;
+
+        const {clientX, clientY} = event;
+        const index = elements.length - 1;
+        const element = createElement(x1, y1, clientX, clientY);
+        console.log(clientX, clientY);
+    };
+
+    const handleMouseUp = () => {
+        setDrawing(false);
+    };
+
     return (
         <canvas
             id="canvas"
             width={window.innerWidth}
             height={window.innerHeight}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={{ handleMouseUp }}
         >
             Canvas
         </canvas>
