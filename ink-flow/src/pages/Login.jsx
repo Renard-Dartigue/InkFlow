d663fb1758d39813d892a9381d16650a0caeac00
Jalene Ferguson <jalene.@Jalenes-MBP.com>
2023-09-12 18:47:40 -0400

    Finished login css.

--- ink-flow/src/pages/Login.jsx
+++ ink-flow/src/pages/Login.jsx
@@ -1,32 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import './Login.css'
 
 const Login = () => {
+
+const [action,setAction] = useState("Login");
+
     return (
-        
-     <div className="section">
+
+        <section>
+    <div className="img-box">
+        <img src="/inkflow-high-resolution-logo-white-on-transparent-background.png"  width={500} height={500} alt="A circle of paint around a paintbrush and Inkflow"/>
+    </div>
+     <div className="input-box">
      <div className="header">
-        <label className="text">Sign Up</label>
+        <label className="text">{action}</label>
      </div>
      <div className="box-1">
     <form className="box-2">
     <div className="input">
-        <label>Email</label>
-        <input type="Email"/>
+        <input type="Email" placeholder="Email"/>
     </div>
     </form>
     <form className="box-2">
     <div className="input">
-    <label>Password</label>
-        <input type="Password"/>
+        <input type="Password" placeholder="Password"/>
     </div>
     </form>
     </div>
     <div className="submit-box">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action==="Login"?"submit blue":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit blue":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
     </div>
      </div>
+     </section>
     )
 }
 
