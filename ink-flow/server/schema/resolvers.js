c15010f1ccad782017e1f7a5cf400b6b605d9040
Renard-Dartigue <136762719+Renard-Dartigue@users.noreply.github.com>
2023-09-12 22:47:22 -0400

    Update resolvers.js

--- ink-flow/server/schema/resolvers.js
+++ ink-flow/server/schema/resolvers.js
@@ -1,20 +1,2 @@
 const { User, Canvas } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-
-const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find().populate('canvases');
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username }).populate('canvases');
-        },
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1 });
-        },
-        thought: async (parent, { thoughtId }) => {
-            return Thought.findOne({ _id: thoughtId });
-        },
-    },
-}
